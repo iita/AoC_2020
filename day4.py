33d223bece679a647e0a9e3bc6f39af77d670ac4
iita <idamaria.lehtinen@iki.fi>
2020-12-04 20:06:41 +0200

    I am an idiot ffs

--- day4.py
+++ day4.py
@@ -2,9 +2,20 @@ import csv
 import re
 import json
 
+required_fields = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
+
+req_dict = {"byr": ":(19[2-9][0-9]|200[0-2])(\s|$)",
+            "iyr": ":(201[0-9]|2020)(\s|$)",
+            "eyr": ":(202[0-9]|2030)(\s|$)",
+            "hgt": ":((1([5-8][0-9]|9[0-3])cm)|((59|6[0-9]|7[0-6])in))(\s|$)",
+            "hcl": ":#[0-9a-f]{6}(\s|$)",
+            "ecl": ":(amb|blu|brn|gry|grn|hzl|oth)(\s|$)",
+            "pid": ":[0-9]{9}(\s|$)"}
+
 
 with open('day4_input.txt', 'r') as f:
-    list_of_sets = []
+    valid_lines = 0
+    valid_2 = 0
     wline = f.readlines()
     wline = ''.join(wline)
     wline = wline.split('\n\n')
@@ -12,12 +23,13 @@ with open('day4_input.txt', 'r') as f:
         l = re.sub('\n', ' ', l)
         l = ' ' + l
         sets = set(re.findall('\s([a-z]+):', l))
-        list_of_sets.append(sets)
+        if(required_fields.issubset(sets)):
+            valid_lines = valid_lines + 1
+            required = 0
+            for r in required_fields:
+                if (re.findall(r + req_dict[r], l)):
+                    required = required + 1
+            if required == 7:
+                valid_2 = valid_2 + 1
 
-required_fields = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
-n_sets = 0
-for s in list_of_sets:
-    if(required_fields.issubset(s)):
-        n_sets = n_sets + 1
-    print(n_sets)
-
+print(valid_lines, valid_2)
