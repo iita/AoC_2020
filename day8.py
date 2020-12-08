7a15ff66e55f5261fba00ff8f276c0aaf7080431
iita <idamaria.lehtinen@iki.fi>
2020-12-08 08:38:59 +0200

    ugly but fast

--- day8.py
+++ day8.py
@@ -26,12 +26,36 @@ def do_things(rown, accumulator):
 used_numbers = []
 accumulated = 0
 rown = 0
+
+last_nr = len(df)-1
+
 def get_things(rown, accumulator):
-    while rown not in used_numbers:
+    while (rown not in used_numbers) and (rown<=last_nr):
         used_numbers.append(rown)
         rown, accumulator = do_things(rown, accumulator)
-        print(rown, accumulator)
-    print(accumulator)
+    if rown > last_nr:
+        print('last_row')
+        print(accumulator)
+        return False
+    return rown, accumulator
 
-get_things(0,0)
-print(used_numbers)
+
+def change_things(n):
+    current_instruct = df.loc[df['rown'] == n, 'instruct'].iloc[0]
+    if current_instruct == "nop":
+        df.loc[(df['rown'] == n), 'instruct'] = "jmp"
+    elif current_instruct == "jmp":
+        df.loc[(df['rown'] == n), 'instruct'] = "nop"
+
+
+print(df.head(10))
+for n in range(len(df)):
+    print(n)
+    used_numbers = []
+    change_things(n)
+    if get_things(0,0) == False:
+        print('found ', n)
+        break
+    else:
+        change_things(n)
+
