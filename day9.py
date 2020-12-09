ba23f76346cf3feaa6f31d56c4c402e1a176a330
iita <idamaria.lehtinen@iki.fi>
2020-12-09 21:34:42 +0200

    part1 done

---
+++ day9.py
@@ -0,0 +1,22 @@
+import csv
+
+test_n = 5
+
+with open('day9_test.txt', 'r') as f:
+    current_n = 0
+    previous_numbers = []
+    fulfills_rule = True
+    fread = csv.reader(f, delimiter=' ')
+    for rown, row in enumerate(fread):
+        current_n = int(row[0])
+        if rown <= test_n-1:
+            previous_numbers.append(current_n)
+        else:
+            for prev_n in range(test_n):
+                if current_n - previous_numbers[prev_n] in previous_numbers[prev_n:]:
+                    break
+                if prev_n == test_n-1:
+                    print(current_n)
+                    break
+            previous_numbers.pop(0)
+            previous_numbers.append(current_n)
